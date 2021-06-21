6561789e3d2ee8b213b16ec38a015cf4f693f982
skylucacius <skylucacius@hotmail.com>
2021-06-21 14:08:12 -0300

    tutorial mosh concluído - sem exercício

---
+++ Mosh/Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //exemplo de rota customizada
+            //routes.MapRoute("MoviesByReleaseDate", "movies/released/{year}/{month}", new { controller = "movies", action = "ByReleaseDate" }, new { year = @"\d{4}", month = @"\d{2}" });
+            //routes.MapRoute("MoviesByReleaseDate2", "movies/released", new { controller = "movies", action = "ByReleaseDate" });
+
+            //rota customizada com atributo
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Mosh/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            //ViewData["movie"] = movie;
+            var customers = new List<Customer> {
+                new Customer() { Name = "Cliente 1", Id = 1},
+                new Customer() { Name = "Cliente 2", Id = 2}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+
+
+
+            //return new EmptyResult();
+            //return HttpNotFound("Página não encontrada!");
+            //return Content("Hello World");
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        public ActionResult Index(int? PageIndex = 10, string sortBy = "teste")
+        {
+            return Content($"pageIndex={PageIndex} e sortBy={sortBy}");
+        }
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
+        public ActionResult ByReleaseDate(int? year, int? month)
+        {
+            if (year == 0 || !year.HasValue) year = DateTime.Now.Year;
+            if (month == 0 || !month.HasValue) month = DateTime.Now.Month;
+
+            return Content($"year={year}, month={month}");
+        }
+
+    }
+}
